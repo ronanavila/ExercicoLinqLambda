70dac4fbda375efdc8da1c511b425088df240de0
Ronan Avila <avila.ronan@gmail.com>
2020-02-06 13:50:28 -0300

    Exceptions Treatment Created

--- DelegateLambdaLinq/Program.cs
+++ DelegateLambdaLinq/Program.cs
@@ -15,35 +15,45 @@ namespace DelegateLambdaLinq
             string path = Console.ReadLine();
             Console.Write("Enter Salary: ");
             double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            
+
 
 
             List<Employee> list = new List<Employee>();
 
-            using (StreamReader sr = File.OpenText(path))
+            try
             {
-                while (!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    string[] data = sr.ReadLine().Split(',');
-                    string name = data[0];
-                    string email = data[1];
-                    double income = double.Parse(data[2], CultureInfo.InvariantCulture);
-                    list.Add(new Employee(name, email, income));
+                    while (!sr.EndOfStream)
+                    {
+                        string[] data = sr.ReadLine().Split(',');
+                        string name = data[0];
+                        string email = data[1];
+                        double income = double.Parse(data[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, income));
+                    }
                 }
+
+
+
+                var result = list.Where(p => p.Salary >= salary).Select(p => p.Email);
+                var startM = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
+
+                Console.WriteLine($"Email of people whose salary is more than " +
+                    $"{salary.ToString("F2", CultureInfo.InvariantCulture)}:");
+
+                foreach (string e in result)
+                {
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': "
+                    + startM.ToString("F2", CultureInfo.InvariantCulture));
             }
-
-            var result = list.Where(p => p.Salary >= salary).Select(p => p.Email);
-            var startM = list.Where(p => p.Name[0] == 'M').Sum(p => p.Salary);
-
-            Console.WriteLine($"Email of people whose salary is more than " +
-                $"{salary.ToString("F2", CultureInfo.InvariantCulture)}:");
-
-            foreach (string e in result)
+            catch (IOException e)
             {
+                Console.WriteLine("An Error Occurred");
                 Console.WriteLine(e);
             }
-            Console.WriteLine("Sum of salary of people whose name starts with 'M': " 
-                +startM.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
